51542ea4ed3a29b2ec44fbea699d0fb97d385c2d
Martha Solomon <marthsolomon@gmail.com>
2021-11-11 22:07:53 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+let num1 = 100
+let num2 = 2000
+const divide = function(){
+    return num2 / num1
+}
+
+const square = value3 => value3*2;
+square(2);
+
+const add = (value4, value5) => value4 + value5;
+add (3, 4);
